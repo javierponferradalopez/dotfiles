6674f720c042cff92317dea13b102a085f641f98
Javier Ponferrada López <javierponferradalopez@gmail.com>
2026-07-08 11:52:18 +0200

    fix(nvim): avoid neo-tree reveal on scratch buffers

--- nvim/.config/nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/custom/plugins/neo-tree.lua
@@ -3,7 +3,17 @@ vim.pack.add {
   'https://github.com/MunifTanjim/nui.nvim',
 }
 
-vim.keymap.set('n', '<leader>e', '<Cmd>Neotree reveal<CR>', { desc = '[E]xplorer reveal current file (focus)' })
+vim.keymap.set('n', '<leader>e', function()
+  -- Only `reveal` when the current buffer is a real file on disk. On scratch
+  -- buffers like the mini.starter dashboard (name `ministarter://1`) reveal
+  -- would prompt to change cwd to a non-existent path, so just focus instead.
+  local path = vim.api.nvim_buf_get_name(0)
+  if path ~= '' and vim.fn.filereadable(path) == 1 then
+    vim.cmd 'Neotree reveal'
+  else
+    vim.cmd 'Neotree focus'
+  end
+end, { desc = '[E]xplorer reveal current file (focus)' })
 vim.keymap.set('n', '<leader>E', '<Cmd>Neotree show<CR>', { desc = '[E]xplorer open (no focus)' })
 
 require('neo-tree').setup {
